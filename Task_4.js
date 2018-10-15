70cee722f2b3a154047f6309986a45e52c451a3a
AnastasyZorina <43998970+AnastasyZorina@users.noreply.github.com>
2018-10-15 10:20:51 +0300

    Create Task_4.js

---
+++ Task_4.js
@@ -0,0 +1,55 @@
+/*
+Задача 4. Присвоить переменной а значение в промежутке [0..15]. С помощью оператора switch организовать вывод чисел от a до 15.*/
+
+var a = prompt ("Задайте число от 0 до 15", '') - 0;
+var j;
+  switch (a) {
+  case 0:
+    for (var j = 0; j <= 15; j++) document.write (j + ' ');
+    break;
+  case 1:
+    for (var j = 1; j <= 15; j++) document.write (j + ' ');
+    break;
+  case 2:
+    for (var j = 2; j <= 15; j++) document.write (j + ' ');
+    break;
+  case 3:
+    for (var j = 3; j <= 15; j++) document.write (j + ' ');
+    break;
+  case 4:
+    for (var j = 4; j <= 15; j++) document.write (j + ' ');
+    break;
+  case 5:
+    for (var j = 5; j <= 15; j++) document.write (j + ' ');
+    break;
+  case 6:
+    for (var j = 6; j <= 15; j++) document.write (j + ' ');
+    break;
+  case 7:
+    for (var j = 7; j <= 15; j++) document.write (j + ' ');
+    break;
+  case 8:
+    for (var j = 8; j <= 15; j++) document.write (j + ' ');
+    break;
+  case 9:
+    for (var j = 9; j <= 15; j++) document.write (j + ' ');
+    break;
+  case 10:
+    for (var j = 10; j <= 15; j++) document.write (j + ' ');
+    break;
+  case 11:
+    for (var j = 11; j <= 15; j++) document.write (j + ' ');
+    break;
+  case 12:
+    for (var j = 12; j <= 15; j++) document.write (j + ' ');
+    break;
+  case 13:
+    for (var j = 13; j <= 15; j++) document.write (j + ' ');
+    break;
+  case 14:
+    for (var j = 14; j <= 15; j++) document.write (j + ' ');
+    break;
+  case 15:
+    for (var j = 15; j <= 15; j++) document.write (j + ' ');
+    break;
+  }
